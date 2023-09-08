8aa7211caa1da04d7638da0a500aa56c05ec6430
Md Sabbirul Latif Shuvo <sabbirul.latif@bjitgroup.com>
2023-09-08 16:28:29 +0600

    updated docker config

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,27 +1,28 @@
 if [ ! -f "vendor/autoload.php" ]; then
-    echo "Installing composer."
-    composer install --no-progress --no-interaction
-    echo "Composer installed."
+  echo "Installing composer."
+  composer install --no-progress --no-interaction
+  echo "Composer installed."
 fi
 
 if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
-    cp .env.example .env
-    # php artisan key:generate
-    php artisan cache:clear
-    php artisan config:clear
-    php artisan route:clear
-    php artisan migrate
+  echo "Creating env file for env $APP_ENV"
+  cp .env.example .env
+  php artisan key:generate
+  php artisan cache:clear
+  php artisan config:clear
+  php artisan route:clear
 else
   echo "env file exists."
   if [ -z "$APP_KEY" ]; then
-    #   php artisan key:generate
+    php artisan key:generate
   else
-     echo "Application key already generated $APP_KEY"
+    echo "Application key already generated $APP_KEY"
   fi
   php artisan cache:clear
   php artisan config:clear
   php artisan route:clear
 fi
+
+php artisan migrate
 
 exec "$@"

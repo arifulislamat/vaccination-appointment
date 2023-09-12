9a91b0f07a9ec627c7c0b5b20664685b0276cd94
Md Sabbirul Latif Shuvo <sabbirul.latif@bjitgroup.com>
2023-09-12 16:31:09 +0600

    added .env

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,20 +10,14 @@ aws_account_id=$(aws ssm get-parameter --with-decryption --name account-id --que
 echo Logging in to Amazon ECR...
 aws ecr get-login-password --region $aws_default_region | docker login --username AWS --password-stdin $aws_account_id.dkr.ecr.$aws_default_region.amazonaws.com
 
-echo Checkout to....
-echo $aws_account_id.dkr.ecr.$aws_default_region.amazonaws.com/vaccination-system-images:$image_tag
-
 docker pull $aws_account_id.dkr.ecr.$aws_default_region.amazonaws.com/vaccination-system-images:$image_tag
 docker tag $aws_account_id.dkr.ecr.$aws_default_region.amazonaws.com/vaccination-system-images:$image_tag $container_name:latest
-
-echo Taging to latest...
-echo $aws_account_id.dkr.ecr.$aws_default_region.amazonaws.com/vaccination-system-images:$image_tag $container_name:latest
 
 docker stop $container_name
 docker rm $container_name
 
-echo pwd
-docker run -d -it -p 8001:19090 --env-file ../.env --name $container_name $container_name:latest
+env_file_path=$(pwd)/../.env
+docker run -d -it -p 8001:19090 --env-file $env_file_path --name $container_name $container_name:latest
 
 # docker compose -f ../docker-compose.prod.yaml down -v
 # docker image prune -f

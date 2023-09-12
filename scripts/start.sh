3fcc5e3aa1f10c1422560e0779f30c2f839ef7b0
Md Sabbirul Latif Shuvo <sabbirul.latif@bjitgroup.com>
2023-09-12 15:17:17 +0600

    updated deploy config

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@
 
 # Container name
 container_name="vaccination-appointment"
+image_tag=appointment-latest
 
 aws_default_region=$(aws ssm get-parameter --name default-region-aws --query "Parameter.Value" --output text)
 aws_account_id=$(aws ssm get-parameter --name account-id --query "Parameter.Value" --output text)
@@ -9,13 +10,19 @@ aws_account_id=$(aws ssm get-parameter --name account-id --query "Parameter.Valu
 echo Logging in to Amazon ECR...
 aws ecr get-login-password --region $aws_default_region | docker login --username AWS --password-stdin $aws_account_id.dkr.ecr.$aws_default_region.amazonaws.com
 
-image_tag=appointment-latest
+echo Checkout to....
+echo $aws_account_id.dkr.ecr.$aws_default_region.amazonaws.com/vaccination-system-images:$image_tag
+
 docker pull $aws_account_id.dkr.ecr.$aws_default_region.amazonaws.com/vaccination-system-images:$image_tag
 docker tag $aws_account_id.dkr.ecr.$aws_default_region.amazonaws.com/vaccination-system-images:$image_tag $container_name:latest
+
+echo Taging to latest...
+echo $aws_account_id.dkr.ecr.$aws_default_region.amazonaws.com/vaccination-system-images:$image_tag $container_name:latest
 
 docker stop $container_name
 docker rm $container_name
 
+echo pwd
 docker run -d -it -p 8001:19090 --env-file ../.env --name $container_name $container_name:latest
 
 # docker compose -f ../docker-compose.prod.yaml down -v

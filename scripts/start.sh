ab8a9adb7bf19ae8d23e02c045d0db756ce5221a
Md Sabbirul Latif Shuvo <sabbirul.latif@bjitgroup.com>
2023-09-12 14:51:13 +0600

    updated deploy config

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Container name
-container_name="vaccination-appointment"
+CONTAINER_NAME="vaccination-appointment"
 
 AWS_DEFAULT_REGION=$(aws ssm get-parameter --name default-region-aws --query "Parameter.Value" --output text)
 AWS_ACCOUNT_ID=$(aws ssm get-parameter --name account-id --query "Parameter.Value" --output text)
@@ -11,12 +11,12 @@ aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --usernam
 
 IMAGE_TAG=appointment-latest
 docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/vaccination-system-images:$IMAGE_TAG
-docker tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/vaccination-system-images:$IMAGE_TAG $container_name:latest
+docker tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/vaccination-system-images:$IMAGE_TAG $CONTAINER_NAME:latest
 
-docker stop $container_name
-docker rm $container_name
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
 
-docker run -d -it -p 8001:19090 --env-file ../.env --name $container_name $container_name:latest
+docker run -d -it -p 8001:19090 --env-file ../.env --name $CONTAINER_NAME $CONTAINER_NAME:latest
 
 # docker compose -f ../docker-compose.prod.yaml down -v
 # docker image prune -f
